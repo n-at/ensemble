8bee556eb35f8bfd5a994910c347118fea85fa7d
Alexey Nurgaliev <atnurgaliev@gmail.com>
2022-04-07 20:42:49 +0300

    storage utils

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/lib/pq v1.10.4
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/viper v1.10.1
+	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29
 )
 
 require (
@@ -26,8 +27,7 @@ require (
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
--- storage/utils.go
+++ storage/utils.go
@@ -1,1 +1,38 @@
 package storage
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"math/rand"
+	"strings"
+)
+
+const PasswordEncryptionCost = 15
+
+func GenerateRandomString(length int) string {
+	vowels := []rune{'e', 'u', 'i', 'o', 'a'}
+	consonants := []rune{'q', 'r', 't', 'p', 's', 'd', 'g', 'h', 'k', 'z', 'x', 'v', 'b', 'n', 'm'}
+
+	str := strings.Builder{}
+
+	for i := 0; i < length; i += 2 {
+		str.WriteRune(consonants[rand.Intn(len(consonants))])
+		if i != length-1 {
+			str.WriteRune(vowels[rand.Intn(len(vowels))])
+		}
+	}
+
+	return str.String()
+}
+
+func EncryptPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), PasswordEncryptionCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func CheckPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
