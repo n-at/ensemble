aed8548b40b633247ea2c5aa929fc1fcdae2475d
Alexey Nurgaliev <atnurgaliev@gmail.com>
2022-04-07 20:09:31 +0300

    database migrations

--- storage/migration.go
+++ storage/migration.go
@@ -25,8 +25,121 @@ func NewMigrator(db *sql.DB) (*Migrator, error) {
 	}
 	return &Migrator{
 		db:         db,
-		migrations: []Migration{},
+		migrations: migrations,
 	}, nil
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+var migrations = []Migration{
+	{
+		version: 1,
+		name:    "users table",
+		query: `
+			create table users (
+				id          varchar(64)     primary key,
+				deleted     boolean,
+				login       varchar(250)    unique not null,
+				password    varchar(250)    not null,
+				role        int             not null
+			)
+		`,
+	}, {
+		version: 2,
+		name:    "users.login index",
+		query:   `create index if not exists users_login on users (login)`,
+	}, {
+		version: 3,
+		name:    "projects table",
+		query: `
+			create table projects (
+				id              varchar(64)     primary key,
+				deleted         boolean,
+				name            varchar(250)    not null,
+				description     text,
+				repo_url        varchar(1000)   not null,
+				repo_branch     varchar(250)    not null,
+				inventory       varchar(250),
+				inventory_list  text,
+				variables       varchar(250),
+				variables_list  text,
+				vault_password  text
+			)
+		`,
+	}, {
+		version: 4,
+		name:    "project updates table",
+		query: `
+			create table project_updates (
+				id          varchar(64)     primary key,
+				project_id  varchar(64)     not null,
+				deleted     boolean,
+				date        timestamp       not null,
+				revision    text,
+				log         text
+			)
+		`,
+	}, {
+		version: 5,
+		name:    "project_updates.project_id index",
+		query:   `create index if not exists project_updates_project_id on project_updates (project_id)`,
+	}, {
+		version: 6,
+		name:    "playbooks table",
+		query: `
+			create table playbooks (
+				id          varchar(64)     primary key,
+				project_id  varchar(64)     not null,
+				deleted     boolean,
+				filename    varchar(250)    not null,
+				name        text,
+				description text,
+				locked      boolean
+			)
+		`,
+	}, {
+		version: 7,
+		name:    "playbooks.project_id index",
+		query:   `create index if not exists playbooks_project_id on playbooks (project_id)`,
+	}, {
+		version: 8,
+		name:    "playbook runs table",
+		query: `
+			create table playbook_runs (
+				id          varchar(64)     primary key,
+				playbook_id varchar(64)     not null,
+				user_id     varchar(64)     not null,
+				deleted     boolean,
+				mode        integer         not null,
+				start_time  timestamp,
+				finish_time timestamp,
+				result      int
+			)
+		`,
+	}, {
+		version: 9,
+		name:    "playbook_runs.playbook_id index",
+		query:   `create index if not exists playbook_runs_playbook_id on playbook_runs (playbook_id)`,
+	}, {
+		version: 10,
+		name:    "playbook_runs.user_id index",
+		query:   `create index if not exists playbook_runs_user_id on playbook_runs (user_id)`,
+	}, {
+		version: 11,
+		name:    "run results table",
+		query: `
+			create table run_results (
+				id      varchar(64) primary key,
+				run_id  varchar(64) not null,
+				deleted boolean,
+				output  text
+			)
+		`,
+	}, {
+		version: 12,
+		name:    "run_results.run_id index",
+		query:   `create index if not exists run_results_run_id on run_results (run_id)`,
+	},
 }
 
 ///////////////////////////////////////////////////////////////////////////////
