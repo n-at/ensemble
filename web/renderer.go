d4259515aeffc55b2f64a6fa442b4973b5c770fa
Alexey Nurgaliev <atnurgaliev@gmail.com>
2024-04-14 13:21:33 +0300

    format output with filters

--- web/renderer.go
+++ web/renderer.go
@@ -7,16 +7,28 @@ import (
 	"fmt"
 	"github.com/flosch/pongo2/v4"
 	"github.com/labstack/echo/v4"
+	log "github.com/sirupsen/logrus"
 	"io"
 	"strings"
 	"time"
 )
 
+const MaxOutputLength = 120
+
 type Pongo2Renderer struct {
 	Debug bool
 }
 
-func (r Pongo2Renderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+func init() {
+	if err := pongo2.RegisterFilter("format_duration", formatDuration); err != nil {
+		log.Errorf("unable to add filter format_duration: %s", err)
+	}
+	if err := pongo2.RegisterFilter("split_output", splitOutput); err != nil {
+		log.Errorf("unable to add filter split_output: %s", err)
+	}
+}
+
+func (r Pongo2Renderer) Render(w io.Writer, name string, data interface{}, _ echo.Context) error {
 	var ctx pongo2.Context
 	var ok bool
 	if data != nil {
@@ -37,14 +49,18 @@ func (r Pongo2Renderer) Render(w io.Writer, name string, data interface{}, c ech
 		return err
 	}
 
-	ctx["format_duration"] = formatDuration
-
 	return t.ExecuteWriter(ctx, w)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-func formatDuration(d time.Duration) string {
+func formatDuration(in *pongo2.Value, _ *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+	if !in.IsInteger() {
+		return pongo2.AsValue("ERR DURATION"), nil
+	}
+
+	d := time.Duration(in.Integer())
+
 	str := strings.Builder{}
 
 	hours := int(d.Hours())
@@ -59,5 +75,36 @@ func formatDuration(d time.Duration) string {
 	}
 	str.WriteString(fmt.Sprintf("%02d.%03ds", seconds, d.Milliseconds()%1000))
 
-	return str.String()
+	return pongo2.AsValue(str.String()), nil
+}
+
+func splitOutput(in *pongo2.Value, _ *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+	if !in.IsString() {
+		return pongo2.AsValue("ERR STR"), nil
+	}
+
+	lines := strings.Split(in.String(), "\n")
+	sb := strings.Builder{}
+
+	for lineIdx, line := range lines {
+		if lineIdx > 0 {
+			sb.WriteRune('\n')
+		}
+
+		r := []rune(line)
+		rlen := len(r)
+
+		for left := 0; left < rlen; left += MaxOutputLength {
+			if left > 0 {
+				sb.WriteString("\\\n")
+			}
+			right := left + MaxOutputLength
+			if right > rlen {
+				right = rlen
+			}
+			sb.WriteString(string(r[left:right]))
+		}
+	}
+
+	return pongo2.AsValue(sb.String()), nil
 }
